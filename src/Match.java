7bda68bfa55f69a0fb0dcda8bd983177af5a4437
leolade <lad.leo@Hotmail.fr>
2016-05-24 18:50:45 +0200

    Fonction de calcul des scores et des calendriers

--- src/Match.java
+++ src/Match.java
@@ -1,5 +1,6 @@
 
 import java.sql.ResultSet;
+import java.util.Calendar;
 
 public class Match {
 
@@ -47,5 +48,92 @@ public class Match {
 
     public boolean egalite() {
         return butEquipe1 == butEquipe2;
+    }
+
+    public static int[] simulerResultat() {
+        int[] res = new int[2];
+        double i = Math.random() * 100;
+        if (i < 30) {
+            res[0] = 0;
+        } else if (i < 65) {
+            res[0] = 1;
+        } else if (i < 85) {
+            res[0] = 2;
+        } else if (i < 92) {
+            res[0] = 3;
+        } else if (i < 97) {
+            res[0] = 4;
+        } else if (i < 98.2) {
+            res[0] = 5;
+        } else if (i < 99) {
+            res[0] = 6;
+        } else if (i < 99.6) {
+            res[0] = 7;
+        } else {
+            res[0] = 8;
+        }
+
+        i = Math.random() * 100;
+
+        if (i < 35) {
+            res[1] = 0;
+        } else if (i < 65) {
+            res[1] = 1;
+        } else if (i < 85) {
+            res[1] = 2;
+        } else if (i < 92) {
+            res[1] = 3;
+        } else if (i < 97) {
+            res[1] = 4;
+        } else if (i < 98.2) {
+            res[1] = 5;
+        } else if (i < 99) {
+            res[1] = 6;
+        } else if (i < 99.6) {
+            res[1] = 7;
+        } else {
+            res[1] = 8;
+        }
+
+        System.out.println(res[0] + " " + res[1]);
+
+        return res;
+    }
+
+    public static String[] simulerDate(int annee) {
+        String[] date = new String[46];
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(annee, 7, 0);
+        while (calendar.get(Calendar.DAY_OF_WEEK) != 7) {
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+        for (int i = 1; i <= 46; i++) {
+            String dateFormater = calendar.get(Calendar.DATE) + "/" + (calendar.get(Calendar.MONTH) + 1) + "/" + calendar.get(Calendar.YEAR);
+            date[i - 1] = dateFormater;
+            calendar.add(Calendar.DATE, 7);
+        }
+        return date;
+    }
+
+    public static String[] simulerDateCoupe(int annee, int numCoupe) {
+        String[] date = new String[6];
+        Calendar calendar = Calendar.getInstance();
+        if (numCoupe == 2) {
+            calendar.set(annee, 9, 1);
+            while (calendar.get(Calendar.DAY_OF_WEEK) != 4) {
+                calendar.add(Calendar.DAY_OF_WEEK, 1);
+            }
+        } else {
+            calendar.set(annee+1, 2, 0);
+            while (calendar.get(Calendar.DAY_OF_WEEK) != 4) {
+                calendar.add(Calendar.DAY_OF_WEEK, 1);
+            }
+        }
+        for (int i = 1; i <= 6; i++) {
+            String dateFormater = calendar.get(Calendar.DATE) + "/" + (calendar.get(Calendar.MONTH) + 1) + "/" + calendar.get(Calendar.YEAR);
+            date[i - 1] = dateFormater;
+            calendar.add(Calendar.DATE, 7);
+        }
+        return date;
     }
 }
