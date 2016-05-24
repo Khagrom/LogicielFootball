066a6b66fbc6685bdfdb83c9ea59fe589e93994b
Vincent <khagrom@gmail.com>
2016-05-24 08:25:51 +0200

    Nettoyage de "Main.java".

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BDD BDD = new BDD();
         ArrayList<String> listeEquipes = new ArrayList<>();
-<<<<<<< HEAD
         int nbChampionnats = 0;
 
         try (ResultSet req = BDD.getStatement().executeQuery("SELECT count(id) FROM championnat;")) {
@@ -24,7 +23,7 @@ public class Main {
                     }
                 }
             }
-            
+
             int nbjour = listeEquipes.size() - 1;
             String[][] matches = new String[((listeEquipes.size()) / 2)][2];
             for (String[] matche : matches) {
@@ -67,63 +66,8 @@ public class Main {
                     }
                 }
             }
-
-        }
-=======
-        ArrayList<String> listeMatchesNonTries = new ArrayList<>();
-        ArrayList<String> listeMatchesTries = new ArrayList<>();
-        int nbChampionnats = 0;
-        Random randomizer = new Random();
-        String eqDom;
-        String eqExt;
-        String match;
-
-        try (ResultSet req = BDD.getStatement().executeQuery("SELECT id FROM championnat;")) {
-            while (req.next()) {
-                nbChampionnats = req.getInt("id");
-            }
         }
 
-        System.out.println("Liste en cours de création...");
-        for (int i = 1; i <= nbChampionnats; i++) {
-
-            try (ResultSet req = BDD.getStatement().executeQuery("SELECT DISTINCT e1.nom, e2.nom"
-                    + " FROM equipe e1, equipe e2"
-                    + " WHERE e1.idChampionnat = " + i
-                    + " AND e2.idChampionnat = " + i
-                    + " AND e1.nom != e2.nom;")) {
-                while (req.next()) {
-                    listeMatchesNonTries.add(req.getString("e1.nom") + " - " + req.getString("e2.nom"));
-                }
-            }
-
-            do {
-                try (ResultSet req = BDD.getStatement().executeQuery("SELECT nom FROM equipe WHERE idChampionnat = " + i + ";")) {
-                    while (req.next()) {
-                        listeEquipes.add(req.getString("nom"));
-                    }
-                }
-
-                do {
-
-                    do {
-                        eqDom = listeEquipes.get(randomizer.nextInt(listeEquipes.size()));
-                        eqExt = listeEquipes.get(randomizer.nextInt(listeEquipes.size()));
-                    } while (eqDom.equals(eqExt));
-                    match = eqDom + " - " + eqExt;
-
-                    if (listeEquipes.remove(eqDom) && listeEquipes.remove(eqExt) && listeMatchesNonTries.remove(match)) {
-                        listeMatchesTries.add(match);
-                    }
-
-                } while (!listeEquipes.isEmpty());
-            } while (!listeMatchesNonTries.isEmpty());
-
-            System.out.println(((double) i / nbChampionnats) * 100 + "%...");
-        }
-        System.out.println();
-        System.out.println("Taille liste : " + listeMatchesTries.size());
->>>>>>> 3944aac810f6f2a3ee2b130137d51b1c151df3f4
         BDD.closeConnection();
     }
 
